04dc8ebba92b0fbc7dce8d1e3d4d4941d9649ee4
jh64 <48374211+jh64@users.noreply.github.com>
2020-10-20 21:39:14 +1100

    refine

--- yml/_deployApp.sh
+++ yml/_deployApp.sh
@@ -3,3 +3,8 @@ clear
 
 kubectl create -f my-app-ws.deployment.prod.yml
 kubectl create -f my-app-ws.load-balancer.prod.yaml
+
+#e.g. started MyAppWsApp in 30.242 seconds (JVM running for 31.662)
+sleep 40
+
+./_getAll.sh
