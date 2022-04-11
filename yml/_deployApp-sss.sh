cfd39e8f194f0f35ed112cb7b0bb7047af925eb4
jh64 <48374211+jh64@users.noreply.github.com>
2022-04-11 13:54:03 +1000

    add deploy script for sss.

---
+++ yml/_deployApp-sss.sh
@@ -0,0 +1,13 @@
+set -v
+clear
+
+#kubectl create -f sss-ws.deployment.prod.yml
+#kubectl create -f sss.load-balancer.prod.yaml
+ kubectl create -f sss-ws.deployment.yml
+ kubectl create -f sss.load-balancer.yaml
+
+#e.g. started MyAppWsApp in 30.242 seconds (JVM running for 31.662)
+#e.g. Started MyAppWsApp in 117.721 seconds (JVM running for 132.297)
+sleep 140
+
+./_getAll.sh
