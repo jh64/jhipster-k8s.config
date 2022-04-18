cb52fe9a70da2aad7e2d2c28ccf06b6333afbfaf
jh64 <48374211+jh64@users.noreply.github.com>
2022-04-18 21:30:47 +1000

    more step

--- yml/_initKubeadm.sh
+++ yml/_initKubeadm.sh
@@ -15,6 +15,9 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+#++++++++++
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
 #++++++++++ please remove the line of 'port 0'
 sudo nano /etc/kubernetes/manifests/kube-scheduler.yaml
 sudo nano /etc/kubernetes/manifests/kube-controller-manager.yaml
