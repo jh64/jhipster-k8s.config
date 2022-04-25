46c3c71c4a0b79bd5559b0c48711a3decc747d51
jh64 <48374211+jh64@users.noreply.github.com>
2022-04-25 23:25:52 +1000

    working now

--- yml/_initKubeadm.sh
+++ yml/_initKubeadm.sh
@@ -6,9 +6,11 @@ sudo kubeadm reset
 sudo swapoff -a 
 
 #++++++++++
-#sudo kubeadm init --apiserver-advertise-address=192.168.2.40 --pod-network-cidr=192.168.0.0/16
+ sudo kubeadm init --apiserver-advertise-address=192.168.2.40 --pod-network-cidr=192.168.0.0/16
 #sudo kubeadm init --pod-network-cidr=10.240.0.0/16
- sudo kubeadm init --apiserver-advertise-address=192.168.2.40 --pod-network-cidr=10.240.0.0/16
+#sudo kubeadm init --apiserver-advertise-address=192.168.2.40 --pod-network-cidr=10.240.0.0/16
+
+read -p 'Press any key to continue...'
 
 #++++++++++
 sudo rm -rf $HOME/.kube
@@ -16,8 +18,13 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+read -p 'Press any key to continue...'
+
 #++++++++++
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+#sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+ sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
+
+read -p 'Press any key to continue...'
 
 #++++++++++ please remove the line of 'port 0'
 sudo nano /etc/kubernetes/manifests/kube-scheduler.yaml
@@ -31,4 +38,4 @@ kubectl get cs
 kubectl get nodes
 
 #++++++++++
-sudo kubeadm token create --print-join-command
+kubeadm token create --print-join-command
